343e6c8cc4d56f2b3eff99a6e0391b7c33f2af2d
smokra <ivan.smokrovic@gmail.com>
2020-02-17 09:25:16 +0100

    Pulling changes

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,7 @@ console.log('Hello world!');
 import smoothscroll from 'smoothscroll-polyfill';
 
 // kick off the polyfill!
+smoothscroll.polyfill();
+
 console.log(smoothscroll)
 console.log(smoothscroll.polyfill())
